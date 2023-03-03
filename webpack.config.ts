d29d4252a3b525837fb3727b0bfcd1ffa2aa6b83
igor <liitvi.igor@gmail.com>
2023-03-03 17:30:43 +0300

    change webpack config to ts version

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+const config: webpack.Configuration = {
   mode: 'production',
   entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
@@ -29,3 +29,5 @@ module.exports = {
     }),
   ],
 };
+
+export default config;
