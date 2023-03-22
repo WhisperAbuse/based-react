d9d44e7b21b4aaf32b6d2fcbffa067b6fd2f7cfd
whisp <incwhisper@gmail.com>
2023-03-22 12:02:46 +0300

    add classNames helper

---
+++ src/helpers/classNames/index.ts
@@ -0,0 +1,15 @@
+export default function cx(
+  className: string,
+  mods: Record<string, boolean>,
+  additional: string[]
+) {
+  const modsCombined = Object.entries(mods)
+    .filter(([, value]) => Boolean(value))
+    .map(([key]) => key);
+
+  const classResult = [className, ...modsCombined, ...additional]
+    .join(" ")
+    .trim();
+
+  return classResult;
+}
